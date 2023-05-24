9926c57da0362d6eb7d99c5553e74a2b9363eeee
Linas Stonkus <liniusst@gmail.com>
2023-05-24 15:06:10 +0300

    uodate for loop

--- task.py
+++ task.py
@@ -1,6 +1,5 @@
 # pylint: disable= missing-docstring
 import string
-import secrets
 import random
 from pymongo import MongoClient
 from pymongo.database import Database
@@ -28,18 +27,17 @@ def create_collection(db_name: str, collection_name: str) -> Database:
 
 
 # sukuriam random fieldus pagal min ir max reiksmes
-def generate_random_data(type_field, min_value, max_value):
-    if type_field == "str":
-        string = "".join(
-            random.choice(string.ascii_lowercase + string.ascii_uppercase)
-            for _ in range(min_value, max_value)
-        )
-        return string
-    elif type_field == "int":
-        random_int = random.uniform(min_value, max_value)
+def generate_random(field_type, min_length, max_length):
+    if field_type == "str":
+        length = random.randint(int(min_length), int(max_length))
+        letters = string.ascii_letters
+        random_string = "".join(random.choice(letters) for _ in range(length))
+        return random_string
+    if field_type == "int":
+        random_int = random.uniform(min_length, max_length)
         return random_int
-    elif type_field == "float":
-        random_float = round(random.uniform(min_value, max_value), 2)
+    if field_type == "float":
+        random_float = round(random.uniform(min_length, max_length), 2)
         return random_float
     else:
         return None
@@ -50,10 +48,11 @@ collection = db["bandymas"]
 
 for _ in range(num_documents):
     document = {}
-    document[field_name] = generate_random_data(field_type, range_min, range_max)
+    document[field_name] = generate_random(field_type, range_min, range_max)
+    print(generate_random(field_type, range_min, range_max))
     collection.insert_one(document)
 
 
 # create_database("task_one")
 # create_collection("task_one", "bandymas")
-generate_random_data(field_type, range_min, range_max)
+# generate_random_data(field_type, range_min, range_max)
